3190968065ee790f064d919e55ca119375a80431
Keane <jinxin0811@126.com>
2021-12-27 13:04:35 +0800

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,10 +6,23 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>@yield('title', 'LaraWeibo')</title>
+  <link rel="stylesheet" href="/css/app.css">
 </head>
 
 <body>
-  @yield('content')
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a href="/" class="navbar-brand">LaraWeibo</a>
+      <ul class="navbar-nav justify-content-end">
+        <li class="nav-item"><a href="/help" class="nav-link">Help</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="container">
+    @yield('content')
+  </div>
 </body>
 
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,12 @@
 @extends('layouts.default')
 @section('content')
-  <h1>Home</h1>
+  <div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+      一切将从这里开始
+    </p>
+    <p>
+      <a href="#" class="btn btn-lg btn-success" role="button">现在注册</a>
+    </p>
+  </div>
 @endsection
