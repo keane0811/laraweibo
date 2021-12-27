4ede7c768b619524a9c91150fe2e0ff5a70528ab
Keane <jinxin0811@126.com>
2021-12-27 17:50:46 +0800

    切割头部和尾部子视图

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,22 +6,17 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>@yield('title', 'LaraWeibo')</title>
-  <link rel="stylesheet" href="/css/app.css">
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-      <a href="/" class="navbar-brand">LaraWeibo</a>
-      <ul class="navbar-nav justify-content-end">
-        <li class="nav-item"><a href="/help" class="nav-link">Help</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
-      </ul>
-    </div>
-  </nav>
+  @include('layouts._header')
 
   <div class="container">
-    @yield('content')
+    <div class="offset-md-1 col-md-10">
+      @yield('content')
+      @include('layouts._footer')
+    </div>
   </div>
 </body>
 
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.default')
 @section('content')
-  <div class="jumbotron">
-    <h1>Hello Laravel</h1>
-    <p class="lead">
-      一切将从这里开始
-    </p>
-    <p>
-      <a href="#" class="btn btn-lg btn-success" role="button">现在注册</a>
-    </p>
+  <div class="p-5 mb-4 bg-light rounded-3">
+    <div class="container py-5">
+      <h1>Hello Laravel</h1>
+      <p class="lead">
+        一切将从这里开始
+      </p>
+      <p>
+        <a href="#" class="btn btn-lg btn-success" role="button">现在注册</a>
+      </p>
+    </div>
   </div>
 @endsection
