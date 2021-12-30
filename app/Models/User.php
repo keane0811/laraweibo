b5446b6a9e54ec75e354215e0bdca10956722ce4
Keane <jinxin0811@126.com>
2021-12-31 02:29:50 +0800

    更改用户资料

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,6 +45,6 @@ class User extends Authenticatable
     public function gravatar($size = 100)
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
-        return "https://dn-qiniu-avatar.qbox.me//avatar/$hash?s=$size";
+        return "https://dn-qiniu-avatar.qbox.me/avatar/$hash?s=$size";
     }
 }
