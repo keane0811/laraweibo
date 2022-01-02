2e91c0444d4c7991e5a354693f75a62fdf1113c5
Keane <jinxin0811@126.com>
2022-01-02 23:08:56 +0800

    管理员可删除用户

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -19,6 +19,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'Keane';
         $user->email = 'keane@126.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
