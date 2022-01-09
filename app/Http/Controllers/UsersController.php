a7286bfdfd1f70f631d279a91dd6460d0f2a9e63
Keane <jinxin0811@126.com>
2022-01-10 02:07:23 +0800

    移除邮件发送的from选项

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -108,13 +108,11 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = 'keane@126.com';
-        $name = 'Keane';
         $to = $user->email;
         $subject = '感谢注册 LaraWeibo ！请确认你的邮箱。';
 
-        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view, $data, function ($message) use ($to, $subject) {
+            $message->to($to)->subject($subject);
         });
     }
 }
