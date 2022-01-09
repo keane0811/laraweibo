8d3936cbe010d3691d6474ca18cc71cf9d61feee
Keane <jinxin0811@126.com>
2022-01-10 04:03:14 +0800

    首页微博列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -62,4 +62,9 @@ class User extends Authenticatable
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "https://dn-qiniu-avatar.qbox.me/avatar/$hash?s=$size";
     }
+
+    public function feed()
+    {
+        return $this->statuses()->orderBy('created_at', 'desc');
+    }
 }
